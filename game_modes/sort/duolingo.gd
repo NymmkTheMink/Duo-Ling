750cc3d5d3d6cb9a17d006812d3bc44b773c37a4
NymmkTheMink <141179443+NymmkTheMink@users.noreply.github.com>
2023-10-27 13:09:54 +0200

    movement + otherstuff

---
+++ game_modes/sort/duolingo.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+
+var current_pos: int
+var pos_1: float = 240.0
+var pos_2: float = 480.0
+var pos_3: float = 720.0
+
+
+func _ready() -> void:
+	# Sets the players starting position to the middle. 
+	current_pos = 2
+
+
+func _physics_process(delta: float) -> void:
+	# sets player position to current position
+	if current_pos == 1:
+		position.x = pos_1
+	elif current_pos == 2:
+		position.x = pos_2
+	else:
+		position.x = pos_3
+	
+	# changes the position when the player moves
+	if Input.is_action_just_pressed("left_P1"):
+		if current_pos != 1:
+			current_pos -= 1
+	elif Input.is_action_just_pressed("right_P1"):
+		if current_pos != 3:
+			current_pos += 1
