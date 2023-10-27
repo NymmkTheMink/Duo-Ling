1358657d390977ed6669c239459cb0a8c577fb50
NymmkTheMink <141179443+NymmkTheMink@users.noreply.github.com>
2023-10-27 13:07:22 +0200

    changed to p2 and changed color

--- game_modes/sort/ling_ling.gd
+++ game_modes/sort/ling_ling.gd
@@ -2,9 +2,9 @@ extends CharacterBody2D
 
 
 var current_pos: int
-var pos_1: float = 240.0
-var pos_2: float = 480.0
-var pos_3: float = 720.0
+var pos_1: float = 1200.0
+var pos_2: float = 1440.0
+var pos_3: float = 1680.0
 
 
 func _ready() -> void:
@@ -13,6 +13,7 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
+	# sets player position to current position
 	if current_pos == 1:
 		position.x = pos_1
 	elif current_pos == 2:
@@ -20,9 +21,10 @@ func _physics_process(delta: float) -> void:
 	else:
 		position.x = pos_3
 	
-	if Input.is_action_just_pressed("left_P1"):
+	# changes the position when the player moves
+	if Input.is_action_just_pressed("left_P2"):
 		if current_pos != 1:
 			current_pos -= 1
-	elif Input.is_action_just_pressed("right_P1"):
+	elif Input.is_action_just_pressed("right_P2"):
 		if current_pos != 3:
 			current_pos += 1
