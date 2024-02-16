0f60642605f4144c582e842c2ff70beea5b8fb3c
NymmkTheMink <141179443+NymmkTheMink@users.noreply.github.com>
2024-02-16 08:36:16 +0100

    endpanel and startpanel stuff

--- game_modes/sort/sort.gd
+++ game_modes/sort/sort.gd
@@ -6,7 +6,10 @@ var player_2_points: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	get_tree().paused = true
+	$StartPanel/Label2.text = str(Global.high_score)
+	$PointsP1/Label.text = "0"
+	$PointsP2/Label.text = "0"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -83,4 +86,26 @@ func _on_spanish_box_p_2_body_entered(body: Node2D) -> void:
 
 
 func _on_timer_timeout() -> void:
+	get_tree().paused = true
+	$EndPanel.visible = true
+	$EndPanel/P1Points.text = str(player_1_points)
+	$EndPanel/P2Points.text = str(player_2_points)
+	if player_1_points > Global.high_score:
+		Global.high_score = player_1_points
+	if player_2_points > Global.high_score:
+		Global.high_score = player_2_points
+
+
+func _on_play_pressed() -> void:
+	get_tree().paused = false
+	$StartPanel.visible = false
+
+
+func _on_replay_button_pressed() -> void:
+	get_tree().paused = false
 	get_tree().reload_current_scene()
+
+
+func _on_back_button_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://main_menu.tscn")
