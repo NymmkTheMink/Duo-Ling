dff6c4a673e5b96e9107b01310c0797aa73e9e7e
medzz1osm <mediz.osm@gmail.com>
2024-03-22 14:44:50 +0100

    word fall down and deliting

--- game_modes/catch/catch.gd
+++ game_modes/catch/catch.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
+var french_word = preload("res://game_modes/catch/words/FrenchWords.tscn") 
+var german_word = preload("res://game_modes/catch/words/GermanWords.tscn")
+var spanish_word = preload("res://game_modes/catch/words/SpanishWords.tscn")
+
+var Words := [french_word, german_word, spanish_word]
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +15,24 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_vanisher_body_entered(body):
+	body.queue_free()
+
+
+func _on_languagebox_ling_body_entered(body):
+	body.queue_free()
+
+
+func _on_languagebox_duo_body_entered(body):
+	body.queue_free()
+
+
+func _on_timer_timeout():
+	randomize()
+	var random_Words = Words[randi() % Words.size()] 
+	var get_Words = random_Words.instantiate()
+	randomize()
+	get_Words.position = Vector2(randi() % (1920/2), 0)
+	$Words.add_child(get_Words)
